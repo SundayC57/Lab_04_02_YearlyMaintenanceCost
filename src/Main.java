ce0e5fe289e2c13665b9179654aa4198d1f6e9f5
SundayC57 <sundaycs@mail.uc.edu>
2026-09-21 18:34:50 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 void main() {
+int inputSpring = 300;
+int inputSummer = 500;
+int inputFall = 200;
+int inputWinter = 600;
 
+int yearlyCost = inputSpring + inputSummer + inputFall + inputWinter;
 
+System.out.println("The yearly maintenance cost is " + yearlyCost + " When the seasonal costs are " + inputSpring + " in spring, "+  inputSummer + " in summer, " + inputFall + " in fall, and " + inputWinter + " in winter.");
 
 
 }
